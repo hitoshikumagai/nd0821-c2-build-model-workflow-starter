6d431e94e0f5d9eebf2ad0ebcdb1bf4ee901518a
hitoshikumagai <78469337+hitoshikumagai@users.noreply.github.com>
2022-02-03 07:37:38 +0900

    bug:Change the path to the path of the folder where the data is stored.

--- main.py
+++ main.py
@@ -135,11 +135,11 @@ def go(config: DictConfig):
             ##################
 
             _ = mlflow.run(
-                os.path.join(hydra.utils.get_original_cwd(), "components", "test_random_forest"),
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
                 "main",
                 parameters={
                     "mlflow_model": "random_forest_export:prod",
-                    "test_dataset": "testval_data.csv:latest",
+                    "test_dataset": "test_data.csv:latest",
                 },
             )
 
